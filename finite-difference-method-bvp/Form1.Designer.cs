628e4665272f84a4d7046fb90fa89997e320b768
Yurij Mikhalevich <0@39.yt>
2014-01-12 13:29:39 +0400

    Initial commit

---
+++ finite-difference-method-bvp/Form1.Designer.cs
@@ -0,0 +1,420 @@
+﻿namespace finite_difference_method_bvp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nSpinBox = new System.Windows.Forms.NumericUpDown();
+            this.dataSet1 = new System.Data.DataSet();
+            this.aSpinBox = new System.Windows.Forms.NumericUpDown();
+            this.bSpinBox = new System.Windows.Forms.NumericUpDown();
+            this.k1SpinBox = new System.Windows.Forms.NumericUpDown();
+            this.k2SpinBox = new System.Windows.Forms.NumericUpDown();
+            this.a1SpinBox = new System.Windows.Forms.NumericUpDown();
+            this.b1SpinBox = new System.Windows.Forms.NumericUpDown();
+            this.l2SpinBox = new System.Windows.Forms.NumericUpDown();
+            this.l1SpinBox = new System.Windows.Forms.NumericUpDown();
+            this.result = new System.Windows.Forms.DataGridView();
+            this.epsMaxTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nSpinBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aSpinBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bSpinBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.k1SpinBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.k2SpinBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.a1SpinBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b1SpinBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.l2SpinBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.l1SpinBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.result)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nSpinBox
+            // 
+            this.nSpinBox.Location = new System.Drawing.Point(129, 12);
+            this.nSpinBox.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nSpinBox.Name = "nSpinBox";
+            this.nSpinBox.Size = new System.Drawing.Size(46, 20);
+            this.nSpinBox.TabIndex = 0;
+            this.nSpinBox.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nSpinBox.ValueChanged += new System.EventHandler(this.nSpinBox_ValueChanged);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // aSpinBox
+            // 
+            this.aSpinBox.DecimalPlaces = 1;
+            this.aSpinBox.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.aSpinBox.Location = new System.Drawing.Point(129, 36);
+            this.aSpinBox.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.aSpinBox.Name = "aSpinBox";
+            this.aSpinBox.Size = new System.Drawing.Size(46, 20);
+            this.aSpinBox.TabIndex = 1;
+            this.aSpinBox.Value = new decimal(new int[] {
+            11,
+            0,
+            0,
+            65536});
+            this.aSpinBox.ValueChanged += new System.EventHandler(this.nSpinBox_ValueChanged);
+            // 
+            // bSpinBox
+            // 
+            this.bSpinBox.DecimalPlaces = 1;
+            this.bSpinBox.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.bSpinBox.Location = new System.Drawing.Point(181, 36);
+            this.bSpinBox.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.bSpinBox.Name = "bSpinBox";
+            this.bSpinBox.Size = new System.Drawing.Size(46, 20);
+            this.bSpinBox.TabIndex = 2;
+            this.bSpinBox.Value = new decimal(new int[] {
+            14,
+            0,
+            0,
+            65536});
+            this.bSpinBox.ValueChanged += new System.EventHandler(this.nSpinBox_ValueChanged);
+            // 
+            // k1SpinBox
+            // 
+            this.k1SpinBox.Location = new System.Drawing.Point(41, 84);
+            this.k1SpinBox.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.k1SpinBox.Name = "k1SpinBox";
+            this.k1SpinBox.Size = new System.Drawing.Size(42, 20);
+            this.k1SpinBox.TabIndex = 3;
+            this.k1SpinBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.k1SpinBox.ValueChanged += new System.EventHandler(this.nSpinBox_ValueChanged);
+            // 
+            // k2SpinBox
+            // 
+            this.k2SpinBox.Location = new System.Drawing.Point(117, 84);
+            this.k2SpinBox.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.k2SpinBox.Name = "k2SpinBox";
+            this.k2SpinBox.Size = new System.Drawing.Size(42, 20);
+            this.k2SpinBox.TabIndex = 4;
+            this.k2SpinBox.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            -2147483648});
+            this.k2SpinBox.ValueChanged += new System.EventHandler(this.nSpinBox_ValueChanged);
+            // 
+            // a1SpinBox
+            // 
+            this.a1SpinBox.Location = new System.Drawing.Point(181, 84);
+            this.a1SpinBox.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.a1SpinBox.Name = "a1SpinBox";
+            this.a1SpinBox.Size = new System.Drawing.Size(42, 20);
+            this.a1SpinBox.TabIndex = 5;
+            this.a1SpinBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            this.a1SpinBox.ValueChanged += new System.EventHandler(this.nSpinBox_ValueChanged);
+            // 
+            // b1SpinBox
+            // 
+            this.b1SpinBox.Location = new System.Drawing.Point(117, 108);
+            this.b1SpinBox.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.b1SpinBox.Name = "b1SpinBox";
+            this.b1SpinBox.Size = new System.Drawing.Size(42, 20);
+            this.b1SpinBox.TabIndex = 8;
+            this.b1SpinBox.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.b1SpinBox.ValueChanged += new System.EventHandler(this.nSpinBox_ValueChanged);
+            // 
+            // l2SpinBox
+            // 
+            this.l2SpinBox.Location = new System.Drawing.Point(41, 108);
+            this.l2SpinBox.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.l2SpinBox.Name = "l2SpinBox";
+            this.l2SpinBox.Size = new System.Drawing.Size(42, 20);
+            this.l2SpinBox.TabIndex = 7;
+            this.l2SpinBox.ValueChanged += new System.EventHandler(this.nSpinBox_ValueChanged);
+            // 
+            // l1SpinBox
+            // 
+            this.l1SpinBox.Location = new System.Drawing.Point(181, 108);
+            this.l1SpinBox.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.l1SpinBox.Name = "l1SpinBox";
+            this.l1SpinBox.Size = new System.Drawing.Size(42, 20);
+            this.l1SpinBox.TabIndex = 6;
+            this.l1SpinBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.l1SpinBox.ValueChanged += new System.EventHandler(this.nSpinBox_ValueChanged);
+            // 
+            // result
+            // 
+            this.result.AllowUserToAddRows = false;
+            this.result.AllowUserToDeleteRows = false;
+            this.result.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.result.Location = new System.Drawing.Point(13, 134);
+            this.result.Name = "result";
+            this.result.ReadOnly = true;
+            this.result.Size = new System.Drawing.Size(573, 220);
+            this.result.TabIndex = 9;
+            // 
+            // epsMaxTextBox
+            // 
+            this.epsMaxTextBox.Location = new System.Drawing.Point(175, 360);
+            this.epsMaxTextBox.Name = "epsMaxTextBox";
+            this.epsMaxTextBox.ReadOnly = true;
+            this.epsMaxTextBox.Size = new System.Drawing.Size(274, 20);
+            this.epsMaxTextBox.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 363);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Максимальная погрешность:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Число точек:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Границы интервала:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Краевые условия:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "k1:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(89, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "k2:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(165, 86);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "a:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 110);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "l1:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(89, 110);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(18, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "l2:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(165, 110);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(16, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "b:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 389);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.epsMaxTextBox);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.b1SpinBox);
+            this.Controls.Add(this.l2SpinBox);
+            this.Controls.Add(this.l1SpinBox);
+            this.Controls.Add(this.a1SpinBox);
+            this.Controls.Add(this.k2SpinBox);
+            this.Controls.Add(this.k1SpinBox);
+            this.Controls.Add(this.bSpinBox);
+            this.Controls.Add(this.aSpinBox);
+            this.Controls.Add(this.nSpinBox);
+            this.MaximumSize = new System.Drawing.Size(606, 423);
+            this.Name = "Form1";
+            this.Text = "Finite Difference Method BVP";
+            ((System.ComponentModel.ISupportInitialize)(this.nSpinBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aSpinBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bSpinBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.k1SpinBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.k2SpinBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.a1SpinBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b1SpinBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.l2SpinBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.l1SpinBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.result)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nSpinBox;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.NumericUpDown aSpinBox;
+        private System.Windows.Forms.NumericUpDown bSpinBox;
+        private System.Windows.Forms.NumericUpDown k1SpinBox;
+        private System.Windows.Forms.NumericUpDown k2SpinBox;
+        private System.Windows.Forms.NumericUpDown a1SpinBox;
+        private System.Windows.Forms.NumericUpDown b1SpinBox;
+        private System.Windows.Forms.NumericUpDown l2SpinBox;
+        private System.Windows.Forms.NumericUpDown l1SpinBox;
+        private System.Windows.Forms.DataGridView result;
+        private System.Windows.Forms.TextBox epsMaxTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+
+    }
+}
+
---
+++ finite-difference-method-bvp/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace finite_difference_method_bvp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            calculate();
+        }
+
+        // Функция коэффициентов при 1-й производной u'(t)
+        private double p(double t)
+        {
+            return Math.Sin(t) / Math.Cos(t);
+        }
+
+        // Функция коэффициентов при u(t)
+        private double g(double t)
+        {
+            return Math.Sqrt(Math.Cos(t));
+        }
+
+        // Функция правой части уравнения
+        private double f(double t)
+        {
+            return 0;
+        }
+
+        // Точное решение yt(t)
+        private double yt(double t)
+        {
+            double c1 = (10 - Math.Cos(Math.Sin(1))) / Math.Sin(Math.Sin(1));
+            return Math.Cos(Math.Sin(t)) + c1 * Math.Sin(Math.Sin(t));
+        }
+
+        private void calculate()
+        {
+            result.DataSource = null;
+            epsMaxTextBox.Clear();
+            int n = (int)nSpinBox.Value + 1; // Число точек + 1
+            double a = (double)aSpinBox.Value; // Границы интервала
+            double b = (double)bSpinBox.Value;
+            if (a >= b)
+            {
+                return;
+            }
+            // Краевые условия
+            int k1 = (int)k1SpinBox.Value;
+            int k2 = (int)k2SpinBox.Value;
+            int a1 = (int)a1SpinBox.Value;
+            int l1 = (int)l1SpinBox.Value;
+            int l2 = (int)l2SpinBox.Value;
+            int b1 = (int)b1SpinBox.Value;
+            double h, eps, epsmax = 0;
+            double[] al = new double[n], bet = new double[n], t = new double[n],
+                u = new double[n], aa = new double[n], bb = new double[n],
+                cc = new double[n], ff = new double[n];
+            int i;
+            DataTable resultData = new DataTable();
+            DataRow row;
+            result.DataSource = resultData;
+            // Вычисление шага сетки
+            h = (b - a) / n;
+            // Формирование векторов коэффициентов для 3-диагональной СЛАУ
+            for (i = 0; i < n; ++i)
+            {
+                t[i] = a + h * i;
+                aa[i] = 1 - p(t[i]) * h / 2;
+                bb[i] = 1 + p(t[i]) * h / 2;
+                cc[i] = 2 - g(t[i]) * h * h;
+                ff[i] = h * h * f(t[i]);
+            }
+            // Краевое условие при t == a
+            al[1] = k2 / (k2 - k1 * h);
+            bet[1] = -(a1 * h) / (k2 - k1 * h);
+            // Прямой ход метода прогонки
+            for (i = 1; i < n - 1; ++i)
+            {
+                al[i + 1] = bb[i] / (cc[i] - al[i] * aa[i]);
+                bet[i + 1] = (aa[i] * bet[i] - ff[i]) / (cc[i] - al[i] * aa[i]);
+            }
+            // Решение на правой границе отрезка
+            u[n - 1] = (l2 * bet[n - 1] + b1 * h) / (l2 + h * l1 - l2 * al[n - 1]);
+            // Обратный ход метода прогонки
+            for (i = n - 2; i >= 0; --i)
+            {
+                u[i] = al[i + 1] * u[i + 1] + bet[i + 1];
+            }
+            // Вывод таблицы и определение макс.погрешности
+            resultData.Columns.Add(new DataColumn("i"));
+            resultData.Columns.Add(new DataColumn("t[i]"));
+            resultData.Columns.Add(new DataColumn("y[i]"));
+            resultData.Columns.Add(new DataColumn("yt[i]"));
+            resultData.Columns.Add(new DataColumn("Погрешность"));
+            for (i = 0; i < n; ++i)
+            {
+                eps = Math.Abs(u[i] - yt(t[i]));
+                if (i == 0 || eps > epsmax)
+                {
+                    epsmax = eps;
+                }
+                row = resultData.NewRow();
+                row["i"] = i;
+                row["t[i]"] = t[i];
+                row["y[i]"] = u[i];
+                row["yt[i]"] = yt(t[i]);
+                row["Погрешность"] = eps;
+                resultData.Rows.Add(row);
+            }
+            epsMaxTextBox.Text = epsmax.ToString();
+        }
+
+        private void nSpinBox_ValueChanged(object sender, EventArgs e)
+        {
+            calculate();
+        }
+    }
+}
